7f871c8d1526ddc65eef7874f30fc000ad299d1c
hyw <727295117@qq.com>
2017-02-20 14:22:45 +0800

    。。

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, hashHistory } from 'react-router';
 
 require('./lib/md5');
-import routes from './app/app-router';
+import routes from './app/app-routing';
 
 // Render the main component into the dom
 ReactDOM.render(<Router routes={routes} history={hashHistory} />, document.getElementById('app'));
